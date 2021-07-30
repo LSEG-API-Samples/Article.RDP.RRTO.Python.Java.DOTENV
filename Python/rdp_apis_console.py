521e3c03242c756f63913283b1ee7f10b044c34e
Wasin Waeosri <wasin.waeosri@refinitiv.com>
2021-07-30 14:30:44 +0700

    add workable python console app

--- Python/rdp_apis_console.py
+++ Python/rdp_apis_console.py
@@ -1,6 +1,8 @@
 import os
 import json
+import sys
 from dotenv import load_dotenv
+from icecream import ic 
 import requests
 
 if __name__ == '__main__':
@@ -8,26 +10,32 @@ if __name__ == '__main__':
     client_secret = ''
     scope = 'trapi'
     universe = '7203.T'
+    auth_obj = None
 
     load_dotenv()
 
-    print(os.getenv('RTO_USER'))
+    # Get RDP Token service information from Environment Variables
+    base_URL = os.getenv('RDP_BASE_URL')
+    auth_endpoint = base_URL + os.getenv('RDP_AUTH_URL') 
 
-    # RDP Login
-    auth_endpoint = os.getenv('RDP_WS_AUTH_URL') 
+     # Get RDP Credentials information from Environment Variables
+    username = os.getenv('RDP_USER')
+    password = os.getenv('RDP_PASSWORD')
+    app_key = os.getenv('RDP_APP_KEY')
+
     # -- Init and Authenticate Session
     auth_request_msg = {
-        'username': os.getenv('RDP_USER') ,
-	    'password': os.getenv('RDP_PASSWORD') ,
+        'username': username ,
+	    'password': 'password' ,
 	    'grant_type': "password",
 	    'scope': scope,
 	    'takeExclusiveSignOnControl': "true"
     }
     
     try:
-        response = requests.post(auth_endpoint, headers = {'Accept':'application/json'}, data = auth_request_msg, auth = (os.getenv('RDP_APP_KEY'), client_secret))
+        response = requests.post(auth_endpoint, headers = {'Accept':'application/json'}, data = auth_request_msg, auth = (app_key, client_secret))
     except Exception as exp:
-        print('Caught exception: %s' % str(exp))
+        ic('Caught exception: %s' % str(exp))
 
     if response.status_code == 200:  # HTTP Status 'OK'
         print('Authentication success')
@@ -35,19 +43,24 @@ if __name__ == '__main__':
     else:
         print('RDP authentication result failure: %s %s' % (response.status_code, response.reason))
         print('Text: %s' % (response.text))
+    
+    if auth_obj is None:
+        print('Authentication fail, exit program')
+        sys.exit(0)
 
-    # ESG Data
-    esg_url = os.getenv('RDP_ESG_URP') 
+    # Get RDP Token service information from Environment Variables
+    esg_url = base_URL + os.getenv('RDP_ESG_URL') 
+
     payload = {'universe': universe}
-
+    esg_object = None
     try:
-        response = requests.get(esg_url, headers={'Authorization': 'Bearer {}'.format(auth_obj['access_token'])}, params = payload)
+        response = requests.get(esg_url, headers={'Authorization': 'Bearer {}'.format(auth_obj['refresh_token'])}, params = payload)
     except Exception as exp:
         print('Caught exception: %s' % str(exp))
 
     if response.status_code == 200:  # HTTP Status 'OK'
-        print('This is a ESG data result from RDP API Call')
-        print(response.json())
+        print('Receive ESG Data from RDP, transform data to Pandas Dataframe format')
+        #print(response.json())
         esg_object=response.json()
     else:
         print('RDP APIs: ESG data request failure: %s %s' % (response.status_code, response.reason))
@@ -55,18 +68,17 @@ if __name__ == '__main__':
 
     print('\n')
 
-    
-
-    # https://developers.refinitiv.com/en/article-catalog/article/using-rdp-api-request-esg-data-jupyter-notebook
-    import pandas as pd
-    import numpy as np
-    headers=esg_object['headers']
-    
-    #Get column headers/titles using lambda
-    titles=map(lambda header:header['title'], headers)
-    
-    dataArray=np.array(esg_object['data'])
-    df=pd.DataFrame(data=dataArray,columns=titles)
-    
-    if df.empty is False:
-        print(df)
+    if esg_object is not None:
+        # https://developers.refinitiv.com/en/article-catalog/article/using-rdp-api-request-esg-data-jupyter-notebook
+        import pandas as pd
+        import numpy as np
+        headers=esg_object['headers']
+        
+        #Get column headers/titles using lambda
+        titles=map(lambda header:header['title'], headers)
+        
+        dataArray=np.array(esg_object['data'])
+        df=pd.DataFrame(data=dataArray,columns=titles)
+        
+        if df.empty is False:
+            print(df)
